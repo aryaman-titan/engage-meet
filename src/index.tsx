741374fd1978535f693af1dfe7a85c40ddbf014d
aryaman-titan <aryamanbehera@gmail.com>
2021-06-26 18:03:17 +0530

    feat(*): added various hooks for state management in the app

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './types'
+import theme from './theme'
+
+import { CssBaseline } from '@material-ui/core';
+import { MuiThemeProvider } from '@material-ui/core/styles';
+
+import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
+
+import NotSupportedWarning from './components/NotSupportedWarning/NotSupportedWarning';
+
+const VideoApp = () => {
+    const { error, setError } = useAppState();
+    const connectionOptions = useConnectionOptions();
+  
+    return (
+      <VideoProvider options={connectionOptions} onError={setError}>
+        <ErrorDialog dismissError={() => setError(null)} error={error} />
+        <ChatProvider>
+          <App />
+        </ChatProvider>
+      </VideoProvider>
+    );
+  };
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <MuiThemeProvider theme={theme}>
+    <CssBaseline />
+    <NotSupportedWarning>
+      <Router>
+        <AppStateProvider>
+          <Switch>
+            <PrivateRoute exact path="/">
+              <VideoApp />
+            </PrivateRoute>
+            <PrivateRoute path="/room/:URLRoomName">
+              <VideoApp />
+            </PrivateRoute>
+            <Route path="/login">
+              <LoginPage />
+            </Route>
+            <Redirect to="/" />
+          </Switch>
+        </AppStateProvider>
+      </Router>
+    </NotSupportedWarning>
+  </MuiThemeProvider>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/state/index.tsx
@@ -0,0 +1,130 @@
+import React, { createContext, useContext, useReducer, useState } from 'react';
+import { RecordingRules, RoomType } from '../types';
+import { TwilioError } from 'twilio-video';
+import { settingsReducer, initialSettings, Settings, SettingsAction } from './settings/settingsReducer';
+import useActiveSinkId from './useActiveSinkId/useActiveSinkId';
+
+export interface StateContextType {
+    error: TwilioError | Error | null;
+    setError(error: TwilioError | Error | null): void;
+    getToken(name: string, room: string, passcode?: string): Promise<{ room_type: RoomType; token: string }>;
+    user?: { displayName: undefined; photoURL: undefined; passcode?: string };
+    signIn?(passcode?: string): Promise<void>;
+    signOut?(): Promise<void>;
+    isAuthReady?: boolean;
+    isFetching: boolean;
+    activeSinkId: string;
+    setActiveSinkId(sinkId: string): void;
+    settings: Settings;
+    dispatchSetting: React.Dispatch<SettingsAction>;
+    roomType?: RoomType;
+    updateRecordingRules(room_sid: string, rules: RecordingRules): Promise<object>;
+}
+
+export const StateContext = createContext<StateContextType>(null!);
+
+export default function AppStateProvider(props: React.PropsWithChildren<{}>) {
+    const [error, setError] = useState<TwilioError | null>(null);
+    const [isFetching, setIsFetching] = useState(false);
+    const [activeSinkId, setActiveSinkId] = useActiveSinkId();
+    const [settings, dispatchSetting] = useReducer(settingsReducer, initialSettings);
+    const [roomType, setRoomType] = useState<RoomType>();
+
+    let contextValue = {
+        error,
+        setError,
+        isFetching,
+        activeSinkId,
+        setActiveSinkId,
+        settings,
+        dispatchSetting,
+        roomType,
+    } as StateContextType;
+
+    contextValue = {
+        ...contextValue,
+        getToken: async (user_identity, room_name) => {
+            const endpoint = process.env.REACT_APP_TOKEN_ENDPOINT || '/token';
+
+            return fetch(endpoint, {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json',
+                },
+                body: JSON.stringify({
+                    user_identity,
+                    room_name,
+                    create_conversation: process.env.REACT_APP_DISABLE_TWILIO_CONVERSATIONS !== 'true',
+                }),
+            }).then(res => res.json());
+        },
+        updateRecordingRules: async (room_sid, rules) => {
+            const endpoint = process.env.REACT_APP_TOKEN_ENDPOINT || '/recordingrules';
+
+            return fetch(endpoint, {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ room_sid, rules }),
+                method: 'POST',
+            })
+                .then(async res => {
+                    const jsonResponse = await res.json();
+
+                    if (!res.ok) {
+                        const recordingError = new Error(
+                            jsonResponse.error?.message || 'There was an error updating recording rules'
+                        );
+                        recordingError.code = jsonResponse.error?.code;
+                        return Promise.reject(recordingError);
+                    }
+
+                    return jsonResponse;
+                })
+                .catch(err => setError(err));
+        },
+    };
+
+    const getToken: StateContextType['getToken'] = async (name, room) => {
+        setIsFetching(true);
+        try {
+            const res = await contextValue
+                .getToken(name, room);
+            setRoomType(res.room_type);
+            setIsFetching(false);
+            return res;
+        } catch (err) {
+            setError(err);
+            setIsFetching(false);
+            return await Promise.reject(err);
+        }
+    };
+
+    const updateRecordingRules: StateContextType['updateRecordingRules'] = async (room_sid, rules) => {
+        setIsFetching(true);
+        try {
+            const res = await contextValue
+                .updateRecordingRules(room_sid, rules);
+            setIsFetching(false);
+            return res;
+        } catch (err) {
+            setError(err);
+            setIsFetching(false);
+            return await Promise.reject(err);
+        }
+    };
+
+    return (
+        <StateContext.Provider value={{ ...contextValue, getToken, updateRecordingRules }}>
+            {props.children}
+        </StateContext.Provider>
+    );
+}
+
+export function useAppState() {
+    const context = useContext(StateContext);
+    if (!context) {
+        throw new Error('useAppState must be used within the AppStateProvider');
+    }
+    return context;
+}
