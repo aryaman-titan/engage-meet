95e3b5f067dc92118534e31fcdf1765b3da53bbd
aryaman-titan <aryamanbehera@gmail.com>
2021-07-01 03:46:41 +0530

    fix(*) : added mobile support and responsive UI

--- src/components/MobileTopMenuBar/MobileTopMenuBar.tsx
+++ src/components/MobileTopMenuBar/MobileTopMenuBar.tsx
@@ -1,46 +1,74 @@
 import { Grid, makeStyles, Theme, Typography } from '@material-ui/core';
-import React from 'react';
+import React, { useState } from 'react';
 import useVideoContext from '../../hooks/useVideoContext/useVideoContext';
 import EndCallButton from '../Buttons/EndCallButton/EndCallButton';
 import Menu from '../MenuBar/Menu/Menu';
+import Snackbar from '../Snackbar/Snackbar';
+import IconButton from '@material-ui/core/IconButton';
+import Tooltip from '@material-ui/core/Tooltip';
+import FileCopyIcon from '@material-ui/icons/FileCopy';
 
 const useStyles = makeStyles((theme: Theme) => ({
-  container: {
-    background: 'white',
-    paddingLeft: '1em',
-    display: 'none',
-    height: `${theme.mobileTopBarHeight}px`,
-    [theme.breakpoints.down('sm')]: {
-      display: 'flex',
+    container: {
+        background: 'white',
+        paddingLeft: '1em',
+        display: 'none',
+        height: `${theme.mobileTopBarHeight}px`,
+        [theme.breakpoints.down('sm')]: {
+            display: 'flex',
+        },
     },
-  },
-  endCallButton: {
-    height: '28px',
-    fontSize: '0.85rem',
-    padding: '0 0.6em',
-  },
-  settingsButton: {
-    [theme.breakpoints.down('sm')]: {
-      height: '28px',
-      minWidth: '28px',
-      border: '1px solid rgb(136, 140, 142)',
-      padding: 0,
-      margin: '0 1em',
+    endCallButton: {
+        height: '28px',
+        fontSize: '0.85rem',
+        padding: '0 0.6em',
     },
-  },
+    settingsButton: {
+        [theme.breakpoints.down('sm')]: {
+            height: '28px',
+            minWidth: '28px',
+            border: '1px solid rgb(136, 140, 142)',
+            padding: 0,
+            margin: '0 1em',
+        },
+    },
 }));
 
 export default function MobileTopMenuBar() {
-  const classes = useStyles();
-  const { room } = useVideoContext();
+    const classes = useStyles();
+    const { room } = useVideoContext();
 
-  return (
-    <Grid container alignItems="center" justify="space-between" className={classes.container}>
-      <Typography variant="subtitle1">{room!.name}</Typography>
-      <div>
-        <EndCallButton className={classes.endCallButton} />
-        <Menu buttonClassName={classes.settingsButton} />
-      </div>
-    </Grid>
-  );
+    const [isCopied, setIsCopied] = useState(false);
+
+    const copyToClipboard = () => {
+        navigator.clipboard.writeText(window.location.href);
+        setIsCopied(true);
+    }
+
+    return (
+        <>
+            <Snackbar
+                open={isCopied}
+                headline="Message"
+                message={"Waiting for the new particpant!"}
+                variant="info"
+                handleClose={() => setIsCopied(false)}
+            />
+            <Grid container alignItems="center" justify="space-between" className={classes.container}>
+                <Typography variant="subtitle1">
+                    <Tooltip title="Copy to clipboard">
+                        <IconButton
+                            aria-label="delete"
+                            onClick={copyToClipboard}>
+                            <FileCopyIcon color="primary" />
+                        </IconButton>
+                    </Tooltip>
+                    {room!.name}</Typography>
+                <div>
+                    <EndCallButton className={classes.endCallButton} />
+                    <Menu buttonClassName={classes.settingsButton} />
+                </div>
+            </Grid>
+        </>
+    );
 }
