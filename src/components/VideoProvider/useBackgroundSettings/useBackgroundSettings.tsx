e71ae8813bdc020bb0e34cc056fbba26248b014e
aryaman-titan <aryamanbehera@gmail.com>
2021-07-11 20:41:18 +0530

    chore(*) : added rimraf and copyfiles as dependency for postinstall script

--- src/components/VideoProvider/useBackgroundSettings/useBackgroundSettings.tsx
+++ src/components/VideoProvider/useBackgroundSettings/useBackgroundSettings.tsx
@@ -1,32 +1,52 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+
 import SanFranciscoThumb from '../../../images/thumb/SanFrancisco.jpg';
+
 import { Thumbnail } from '../../BackgroundSelectionDialog/BackgroundThumbnail/BackgroundThumbnail';
 
+import { VirtualBackgroundProcessor } from '@twilio/video-processors';
+
+import useLocalVideoToggle from "../../../hooks/useLocalVideoToggle/useLocalVideoToggle";
 export interface BackgroundSettings {
-  type: Thumbnail;
-  index?: number;
+    type: Thumbnail;
+    index?: number;
 }
 
 const imageNames: string[] = [
-  'San Francisco',
+    'San Francisco',
 ];
 
 const images = [
-  SanFranciscoThumb,
+    SanFranciscoThumb,
 ];
 
 export const backgroundConfig = {
-  imageNames,
-  images,
+    imageNames,
+    images,
 };
 
 // TODO : Add video processing logic after backgroundSettings change
 // useEffect hooks, etc ...
+const assetsPath = '/virtualbackground'
 
 export default function useBackgroundSettings() {
-  const [backgroundSettings, setBackgroundSettings] = useState({
-    type: 'none',
-    index: 0,
-  } as BackgroundSettings);
-  return [backgroundSettings, setBackgroundSettings] as const;
+
+    const [isVideoEnabled, toggleVideoEnabled] = useLocalVideoToggle();
+
+    const [backgroundSettings, setBackgroundSettings] = useState({
+        type: 'none',
+        index: 0,
+    } as BackgroundSettings);
+
+    useEffect(() => {
+        if (backgroundSettings.type !== 'none') {
+            const virtualBackgroundProcessor = new VirtualBackgroundProcessor({
+                assetsPath,
+                backgroundImage,
+                fitType,
+              });
+        }
+    }, [backgroundSettings])
+
+    return [backgroundSettings, setBackgroundSettings] as const;
 }
--- src/hooks/useLocalVideoToggle/useLocalVideoToggle.tsx
+++ src/hooks/useLocalVideoToggle/useLocalVideoToggle.tsx
@@ -1,28 +1,44 @@
-import { LocalVideoTrack } from 'twilio-video';
+import { LocalVideoTrack, VideoProcessor } from 'twilio-video';
 import { useCallback, useState } from 'react';
 import useVideoContext from '../useVideoContext/useVideoContext';
 
+
 export default function useLocalVideoToggle() {
-  const { room, localTracks, getLocalVideoTrack, removeLocalVideoTrack, onError } = useVideoContext();
-  const localParticipant = room?.localParticipant;
-  const videoTrack = localTracks.find(track => track.name.includes('camera')) as LocalVideoTrack;
-  const [isPublishing, setIspublishing] = useState(false);
+    const { room, localTracks, getLocalVideoTrack, removeLocalVideoTrack, onError } = useVideoContext();
+    const localParticipant = room?.localParticipant;
+    const videoTrack = localTracks.find(track => track.name.includes('camera')) as LocalVideoTrack;
+    const [isPublishing, setIspublishing] = useState(false);
 
-  const toggleVideoEnabled = useCallback(() => {
-    if (!isPublishing) {
-      if (videoTrack) {
-        const localTrackPublication = localParticipant?.unpublishTrack(videoTrack);
-        localParticipant?.emit('trackUnpublished', localTrackPublication);
-        removeLocalVideoTrack();
-      } else {
-        setIspublishing(true);
-        getLocalVideoTrack()
-          .then((track: LocalVideoTrack) => localParticipant?.publishTrack(track, { priority: 'low' }))
-          .catch(onError)
-          .finally(() => setIspublishing(false));
-      }
-    }
-  }, [videoTrack, localParticipant, getLocalVideoTrack, isPublishing, onError, removeLocalVideoTrack]);
+    const setProcessor = (processor: VideoProcessor, track: LocalVideoTrack) => {
+        if (track.processor) {
+            track.removeProcessor(track.processor);
+        }
+        if (processor) {
+            track.addProcessor(processor);
+        }
+    };
 
-  return [!!videoTrack, toggleVideoEnabled] as const;
+    const toggleVideoEnabled = useCallback((processor?: VideoProcessor ) => {
+        if (!isPublishing) {
+            if (videoTrack) {
+                const localTrackPublication = localParticipant?.unpublishTrack(videoTrack);
+                localParticipant?.emit('trackUnpublished', localTrackPublication);
+                removeLocalVideoTrack();
+            } else {
+                setIspublishing(true);
+                getLocalVideoTrack()
+                    .then((track: LocalVideoTrack) => {
+                        if(processor){
+
+                            setProcessor(processor, track);
+                        }
+                        localParticipant?.publishTrack(track, { priority: 'low' })
+                    })
+                    .catch(onError)
+                    .finally(() => setIspublishing(false));
+            }
+        }
+    }, [videoTrack, localParticipant, getLocalVideoTrack, isPublishing, onError, removeLocalVideoTrack]);
+
+    return [!!videoTrack, toggleVideoEnabled] as const;
 }
